da67ab27c90465fecd78c8a2d59502cba6a5f54d
Pablo Ndure <ndurepablo@gmail.com>
2023-11-26 19:32:32 -0300

    create model db

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def category():
 
     return render_template('category.html', table_data=table_data, headers=headers)
 
+with app.app_context():
+    db.create_all()
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=False)
